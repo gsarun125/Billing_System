903bea400dc4b80e798f25fc01203c49911d276d
gsarun125 <127116889+gsarun125@users.noreply.github.com>
2023-07-25 16:08:13 +0530

    something done

--- app/src/main/java/com/mini/billingsystem/Activity/SalesActivity.java
+++ app/src/main/java/com/mini/billingsystem/Activity/SalesActivity.java
@@ -2,17 +2,104 @@ package com.mini.billingsystem.Activity;
 
 
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
 
 import com.mini.billingsystem.Activity.DrawerBaseActivity;
 import com.mini.billingsystem.databinding.ActivitySalesBinding;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class SalesActivity extends DrawerBaseActivity {
     ActivitySalesBinding activitySalesBinding;
+    int pageWidth=1200;
+    String Customer_Name="ARUN G";
+    String PHone_NO="9443528960";
+    String Bill_NO="CA1937";
+    String Date="25/07/23";
+
+    String Time="01:29 PM";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activitySalesBinding = ActivitySalesBinding.inflate(getLayoutInflater());
         setContentView(activitySalesBinding.getRoot());
+        activitySalesBinding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PdfDocument document=new PdfDocument();
+                Paint myPaint=new Paint();
+                Paint titlePaint=new Paint();
+
+                PdfDocument.PageInfo myPageInfo1=new PdfDocument.PageInfo.Builder(1200,2010,1).create();
+                PdfDocument.Page page=document.startPage(myPageInfo1);
+                Canvas canvas=page.getCanvas();
+
+                //title
+                titlePaint.setTextAlign(Paint.Align.CENTER);
+                titlePaint.setTypeface(Typeface.create(Typeface.DEFAULT,Typeface.BOLD));
+                titlePaint.setTextSize(70);
+                canvas.drawText("Shopping Center",pageWidth/2,270,titlePaint);
+
+                myPaint.setColor(Color.rgb(0,133,200));
+                myPaint.setTextSize(30);
+                myPaint.setTextAlign(Paint.Align.RIGHT);
+                canvas.drawText("Call:9443528960",1160,40,myPaint);
+
+                titlePaint.setTextAlign(Paint.Align.CENTER);
+                titlePaint.setTypeface(Typeface.create(Typeface.DEFAULT,Typeface.ITALIC));
+                titlePaint.setTextSize(70);
+                canvas.drawText("--------------------------------------------------",pageWidth/2,400,titlePaint);
+
+
+                titlePaint.setTextAlign(Paint.Align.CENTER);
+                titlePaint.setTypeface(Typeface.create(Typeface.DEFAULT,Typeface.ITALIC));
+                titlePaint.setTextSize(70);
+                canvas.drawText("Invoice",pageWidth/2,500,titlePaint);
+
+                myPaint.setTextAlign(Paint.Align.LEFT);
+                myPaint.setTextSize(35f);
+                myPaint.setColor(Color.BLACK);
+                canvas.drawText("Customer Name: "+Customer_Name,20,590,myPaint);
+                canvas.drawText("Phone No: "+PHone_NO,20,650,myPaint);
+
+                myPaint.setTextAlign(Paint.Align.LEFT);
+                myPaint.setTextSize(35f);
+                myPaint.setColor(Color.BLACK);
+                canvas.drawText("Bill No : "+Bill_NO,pageWidth-20,590,myPaint);
+                canvas.drawText("Date : "+Date,pageWidth-20,650,myPaint);
+
+
+                document.finishPage(page);
+
+                File downloadsDir= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                String fileName="stock.pdf";
+                File file=new File(downloadsDir,fileName);
+                try {
+                    FileOutputStream fos=new FileOutputStream(file);
+                    document.writeTo(fos);
+                    document.close();
+                    fos.close();
+                    Toast.makeText(SalesActivity.this,"Successfully",Toast.LENGTH_SHORT).show();
+
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/mini/billingsystem/Activity/StockActivity.java
+++ app/src/main/java/com/mini/billingsystem/Activity/StockActivity.java
@@ -6,7 +6,19 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.util.LruCache;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.mini.billingsystem.DataBase.DataBaseHandler;
 import com.mini.billingsystem.DataBase.DataBaseHandlerKt;
@@ -14,12 +26,17 @@ import com.mini.billingsystem.R;
 import com.mini.billingsystem.databinding.ActivityStockBinding;
 import com.mini.billingsystem.model.NoteAdapter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class StockActivity extends DrawerBaseActivity {
     private RecyclerView recyclerView;
+    private File filePDFOutput;
     private List<String> mProduct_ID = new ArrayList();
     private List<String> mProduct_Name = new ArrayList();
     private List<String> mQuantity = new ArrayList();
@@ -38,6 +55,64 @@ public class StockActivity extends DrawerBaseActivity {
         mCost.add("cost");
 
         Refresh_Feed();
+        Binding.Imagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("working");
+                convertXMltoPDF();
+
+            }
+        });
+    }
+    void convertXMltoPDF(){
+
+
+        View view= LayoutInflater.from(this).inflate(R.layout.activity_stock,null);
+        DisplayMetrics displayMetrics=new DisplayMetrics();
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+            this.getDisplay().getRealMetrics(displayMetrics);
+        }
+        else{
+            this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        }
+        view.measure(View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels,View.MeasureSpec.EXACTLY),View.MeasureSpec.makeMeasureSpec(displayMetrics.heightPixels,View.MeasureSpec.EXACTLY));
+        view.layout(0,0,displayMetrics.widthPixels,displayMetrics.heightPixels);
+
+
+        Bitmap recycler_view_bm =     getScreenshotFromRecyclerView(recyclerView);
+        PdfDocument document=new PdfDocument();
+        int viewWidth=view.getMeasuredWidth();
+        int viewHight=view.getMeasuredHeight();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(recycler_view_bm.getWidth(),
+                recycler_view_bm.getHeight(),
+                9).create();
+            recycler_view_bm.prepareToDraw();
+         PdfDocument.Page page=document.startPage(pageInfo);
+
+        Canvas canvas=page.getCanvas();
+        canvas.drawBitmap(recycler_view_bm,0,0,null);
+        recyclerView.draw(canvas);
+        document.finishPage(page);
+
+
+
+
+        File downloadsDir= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String fileName="stock.pdf";
+        File file=new File(downloadsDir,fileName);
+        try {
+            FileOutputStream fos=new FileOutputStream(file);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            Toast.makeText(this,"Successfully",Toast.LENGTH_SHORT).show();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     void Refresh_Feed(){
         Cursor c1 = db.get_value("SELECT  * FROM Stock");
@@ -64,4 +139,48 @@ public class StockActivity extends DrawerBaseActivity {
 
     }
 
+    public Bitmap getScreenshotFromRecyclerView(RecyclerView view) {
+        RecyclerView.Adapter adapter = view.getAdapter();
+        Bitmap bigBitmap = null;
+        if (adapter != null) {
+            int size = adapter.getItemCount();
+            int height = 0;
+            Paint paint = new Paint();
+            int iHeight = 0;
+            final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+            // Use 1/8th of the available memory for this memory cache.
+            final int cacheSize = maxMemory / 8;
+            LruCache<String, Bitmap> bitmaCache = new LruCache<>(cacheSize);
+            for (int i = 0; i < size; i++) {
+                RecyclerView.ViewHolder holder = adapter.createViewHolder(view, adapter.getItemViewType(i));
+                adapter.onBindViewHolder(holder, i);
+                holder.itemView.measure(View.MeasureSpec.makeMeasureSpec(view.getWidth(), View.MeasureSpec.EXACTLY),
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+                holder.itemView.layout(0, 0, holder.itemView.getMeasuredWidth(), holder.itemView.getMeasuredHeight());
+                holder.itemView.setDrawingCacheEnabled(true);
+                holder.itemView.buildDrawingCache();
+                Bitmap drawingCache = holder.itemView.getDrawingCache();
+                if (drawingCache != null) {
+
+                    bitmaCache.put(String.valueOf(i), drawingCache);
+                }
+
+                height += holder.itemView.getMeasuredHeight();
+            }
+
+            bigBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), height, Bitmap.Config.ARGB_8888);
+            Canvas bigCanvas = new Canvas(bigBitmap);
+            bigCanvas.drawColor(Color.WHITE);
+
+            for (int i = 0; i < size; i++) {
+                Bitmap bitmap = bitmaCache.get(String.valueOf(i));
+                bigCanvas.drawBitmap(bitmap, 0f, iHeight, paint);
+                iHeight += bitmap.getHeight();
+                bitmap.recycle();
+            }
+
+        }
+        return bigBitmap;
+    }
 }
